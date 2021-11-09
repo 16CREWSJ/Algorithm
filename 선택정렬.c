5fd335310726847cd6bfe6da8a02c414d15f190e
16CREWSJ <90380770+16CREWSJ@users.noreply.github.com>
2021-11-09 16:30:01 +0900

    Add files via upload

---
+++ 선택정렬.c
@@ -0,0 +1,30 @@
+/*
+	어떤 숫자들을 오름차순으로 정렬할 때, 
+	가장 작은 값을 선택해서 제일 앞으로 보내는 알고리즘 
+*/
+
+#include<stdio.h>
+
+int main(void){
+	int arr[] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+	int i, j, min, index, temp;
+	
+	for(i=0; i<sizeof(arr)/sizeof(int); i++){
+		min = 9999;
+		
+		for(j = i; j<sizeof(arr)/sizeof(int); j++){
+			if(arr[j] < min){
+				min = arr[j];
+				index = j;
+			}
+		}
+		temp = arr[i];
+		arr[i] = arr[index];
+		arr[index] = temp;
+	}
+	
+	for(i=0; i<sizeof(arr)/sizeof(int); i++)
+		printf("%d ", arr[i]);
+		
+	return 0;
+}
