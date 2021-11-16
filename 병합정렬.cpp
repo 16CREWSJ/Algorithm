2673e4e7e21e2f62c0b8e638792f71b905760a2c
16CREWSJ <90380770+16CREWSJ@users.noreply.github.com>
2021-11-16 23:29:19 +0900

    Add files via upload

---
+++ 병합정렬.cpp
@@ -0,0 +1,66 @@
+/*
+	병합 정렬은 일단 반으로 나누고 나중에 합쳐서 정렬하는 알고리즘 
+	분할 정복 방법
+	퀵 정렬은 피벗 값에 따라 편향되게 분할할 가능성이 있음
+	하지만 병합 정렬은 정확히 반절씩 나누기 때문에 항상 같은 시간 복잡도를 가짐 
+	시간 복잡도 O(N * logN) 
+*/
+
+#include<stdio.h>
+
+int number = 8;
+int sorted[8]; // 정렬 배열은 반드시 전역 변수로 선언(불필요한 메모리 사용 방지) 
+
+void merge(int a[], int m, int middle, int n){
+	int i = m;
+	int j = middle + 1;
+	int k = m;
+	// 작은 순서대로 배열에 삽입
+	while(i <= middle && j <= n){
+		if(a[i] <= a[j]){
+			sorted[k] = a[i];
+			i++;
+		} else{
+			sorted[k] = a[j];
+			j++;
+		}
+		k++;
+	} 
+	// 남은 데이터도 삽입
+	if(i > middle){
+		for(int t = j; t <= n; t++){
+			sorted[k] = a[t];
+			k++;
+		}
+	} else{
+		for(int t = i; t <= middle; t++){
+			sorted[k] = a[t];
+			k++;
+		}
+	}
+	// 정렬된 배열을 삽입
+	for(int t = m; t <= n; t++){
+		a[t] = sorted[t];
+	} 
+}
+
+void mergeSort(int a[], int m, int n){
+	// 크기가 1보다 큰 경우
+	if(m < n){
+		int middle = (m + n) / 2;
+		mergeSort(a, m, middle);
+		mergeSort(a, middle + 1, n);
+		merge(a, m, middle, n);
+	} 
+}
+
+int main(void){
+	int array[number] = {7, 6, 5, 8, 3, 9, 5, 1};
+	mergeSort(array, 0, number - 1);
+	
+	for(int i = 0; i < number; i++){
+		printf("%d ", array[i]);
+	}
+	
+	return 0;
+}
